3d79947c554e14932fa603751af890a056a41e21
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-11-15 20:23:57 -0300

    main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import "./main.css";
+import cartas from "./../main/cartas.jpg";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <section id="home" className="sectiona">
+        <div className="container">
+          <div>
+            <h1 className="container__h1">Hola, soy Paula Dominguez</h1>
+            <p>
+              Let the MADNESS begin! Never let good enough BE enough! Hard work
+              beats talent when talent doesn't work hard. Winning is a habit,
+              Success is a choice. With this slogan, we look forward to provide
+              you every sort of Ball Equipments, that too, the BEST.
+            </p>
+            <a href="#" className="btn">
+              Read More
+            </a>
+          </div>
+          <img id="header-img" src={cartas} alt="cartas" />
+        </div>
+      </section>
+
+      <section id="about" className="sectionb">
+        <div className="overlay">
+          <div className="sectionb-inner">
+            <h3>Swish all em Threes</h3>
+            <h2>Just Get Down Hooping On the Court</h2>
+            <p>
+              From A Basketball to Shooting Sleeves, everything you need - As a
+              beginner, amateur or a pro - The one stop Ball solution for every
+              baller.
+              <br />
+              Visit your nearest SamiHoops store today!
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Main;
