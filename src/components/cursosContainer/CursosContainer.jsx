b061ae4deb4aed40a0915dd8cd1648e0aa7d8aab
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-11-16 23:30:58 -0300

    curso component

---
+++ src/components/cursosContainer/CursosContainer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+let cursosArray = [
+  {
+    curso: "Arcanos mayores",
+    clases: 8,
+    duración: "256min",
+    precio: 16000,
+  },
+  {
+    curso: "Arcanos menores",
+    clases: 8,
+    duración: "256min",
+    precio: 16000,
+  },
+  {
+    curso: "Numerologia",
+    clases: 6,
+    duración: "226min",
+    precio: 13000,
+  },
+];
+
+const CursosContainer = () => {
+  return <div>CursosContainer</div>;
+};
+
+export default CursosContainer;
