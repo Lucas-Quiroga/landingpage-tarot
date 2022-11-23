5c7eaab536a24d95ea598c8f53b02330c0d2dfcb
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-11-23 20:23:38 -0300

    probando

--- src/components/cursosContainer/cursosMap/cursosCards/CursosCards.jsx
+++ src/components/cursosContainer/cursosMap/cursosCards/CursosCards.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import "./cursosCard.css";
 
 const CursosCards = ({ cursos }) => {
   return (
     <div>
       <div className="cards__container">
-        <h1>{cursos.curso}</h1>
-        <h3>Clases: {cursos.clases}</h3>
-        <h4>Duración: {cursos.duración}</h4>
-        <h2>Precio: {cursos.precio}</h2>
-        <a href="https://mpago.la/2YrKJ84" target="_blank">
-          <button>Comprar</button>
-        </a>
+        <div className="cards__grid">
+          <h1>{cursos.curso}</h1>
+          <h3>Clases: {cursos.clases}</h3>
+          <h4>Duración: {cursos.duración}</h4>
+          <h2>Precio: {cursos.precio}</h2>
+          <a href="https://mpago.la/2YrKJ84" target="_blank">
+            <span className="cards__info">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
+              error optio commodi voluptatum reiciendis cupiditate? Asperiores,
+              est quod. Quis, porro?
+            </span>
+            <button>Comprar</button>
+          </a>
+        </div>
       </div>
     </div>
   );
