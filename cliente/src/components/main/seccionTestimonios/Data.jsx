1c1ea650727db9787c9fd32a11bf7ebfb2242a2b
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-09-05 23:56:49 -0300

    deploy

--- cliente/src/components/main/seccionTestimonios/Data.jsx
+++ cliente/src/components/main/seccionTestimonios/Data.jsx
@@ -2,6 +2,7 @@ import Image1 from "./../../../assets/camilapablos.png";
 import Image2 from "./../../../assets/isabelsilva.png";
 import Image3 from "./../../../assets/yannina.png";
 import Image4 from "./../../../assets/marcela.png";
+import Image5 from "./../../../assets/sabrina.png";
 
 export const Data = [
   {
@@ -32,13 +33,13 @@ export const Data = [
     description:
       "Hola, mi nombre es Marcela y soy alumna de Paula. Me encanta porque sus clases son dinámicas, y ella siempre está dispuesta a brindarnos todos sus conocimientos y aclararnos dudas no solo durante las clases, sino en cualquier momento.",
   },
-  // {
-  //   id: 5,
-  //   image: Image3,
-  //   title: "CR7",
-  //   description:
-  //     "A really good job, all aspects of the project were followed step by step and with good results.",
-  // },
+  {
+    id: 5,
+    image: Image5,
+    title: "Sabrina Seijo",
+    description:
+      "Comencé como paciente y quedé cautivada por su desempeño y exactitud en sus tiradas. Tanto así que decidí convertirme en su alumna y adentrarme de su mano en este maravilloso mundo! Gracias Pau por tantos conocimientos y hermosas experiencias, por tus palabras justas en cada situación y por estar siempre cerca cuando te necesito! Sos una excelente profesional con un brillo excepcional, te quiero mucho ❤️",
+  },
   // {
   //   id: 6,
   //   image: Image3,
